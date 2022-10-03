f5f10c28d0d1bb450236936b457e506b34a2487e
Nick <nlchan168@yahoo.com>
2022-10-03 03:15:05 -0400

    basic bot deployment

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-import express from "express";
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+// Login to Discord with your client's token
+client.login(process.env.DISCORD_TOKEN);
