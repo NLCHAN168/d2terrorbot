40cb7869a57f099da9c1e9f39963dcd394eede88
Nick <nlchan168@yahoo.com>
2022-10-03 03:39:34 -0400

    added slash commands

--- main.js
+++ main.js
@@ -10,5 +10,23 @@ client.once("ready", () => {
   console.log("Ready!");
 });
 
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply(
+      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nGuild Created: ${interaction.guild.createdAt}`
+    );
+  } else if (commandName === "user") {
+    await interaction.reply(
+      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
+    );
+  }
+});
+
 // Login to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
