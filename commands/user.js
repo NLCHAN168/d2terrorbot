d433f8db99a28621cd179ed2be25161bfe36bfbe
Nick <nlchan168@yahoo.com>
2022-10-04 04:30:53 -0400

    added modular command functionality

---
+++ commands/user.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("user")
+    .setDescription("Displays user info"),
+  async execute(interaction) {
+    await interaction.reply(
+      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
+    );
+  },
+};
--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 // Require the necessary discord.js classes
+const fs = require("node:fs");
+const path = require("node:path");
+const { Collection } = require("discord.js");
 const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
@@ -13,20 +29,19 @@ client.once("ready", () => {
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isChatInputCommand()) return;
 
-  const { commandName } = interaction;
+  const command = interaction.client.commands.get(interaction.commandName);
 
-  if (commandName === "ping") {
-    await interaction.reply("Pong!");
-  } else if (commandName === "server") {
-    await interaction.reply(
-      `Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}\nGuild Created: ${interaction.guild.createdAt}`
-    );
-  } else if (commandName === "user") {
-    await interaction.reply(
-      `Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`
-    );
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
   }
 });
-
 // Login to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN);
